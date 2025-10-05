b32a8b3b95c7e883e5e9ffa6079a282f7ef3ec28
lolguy91 <retek1544@gmail.com>
2025-10-05 18:00:58 +0200

    MSVC compat incase somebody needs it sometime in the future

--- test/util.h
+++ test/util.h
@@ -1,6 +1,11 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
-#define UNUSED __attribute__((unused))
+#if defined(_MSC_VER)
+  #define UNUSED
+#elif defined(__GNUC__) || defined(__clang__)
+  #define UNUSED __attribute__((unused))
+#else
+  #define UNUSED
 
 #endif // __UTIL_H__
